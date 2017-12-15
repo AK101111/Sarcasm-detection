7dff23a3fbe016c20b88dc7a3a80d2de06a13bea
AK101111 <arnavkansal@gmail.com>
2017-12-14 22:26:49 -0500

    all clean data

--- dataToData.py
+++ dataToData.py
@@ -3,22 +3,20 @@ import re
 import string
 import preprocessor
 
-DATADIR = '/Users/arnavkansal/Desktop/Courant/Fall17/StatNLP/Sarcasm/twitter-to-csv-master/'
-dataFiles = [	'#ironical_since_2013-10-01_until_2017-10-31', 
-				'#ironical_since_2017-10-31_until_2017-12-05', 
-				'#irony_since_2013-10-01_until_2017-10-31',
-				'#irony_since_2017-10-31_until_2017-12-05' ,
-				'#sarcasm_since_2013-10-01_until_2017-10-31', 
+DATADIR = '/Users/arnavkansal/Desktop/Courant/Fall17/StatNLP/Sarcasm/data/'
+dataFiles = [	'#sarcasm_since_2013-10-01_until_2017-10-31', 
 				'#sarcasm_since_2017-10-31_until_2017-12-05',
+				'#sarcasm_since_2017-12-05_until_2017-12-14', 
 				'#sarcastic_since_2013-10-01_until_2017-10-31', 
-				'#sarcastic_since_2017-10-31_until_2017-12-05']
-dataFilesI = [	'#sarcasm_since_2013-10-01_until_2017-10-31', 
-				'#sarcasm_since_2017-10-31_until_2017-12-05',
-				'#sarcastic_since_2013-10-01_until_2017-10-31', 
-				'#sarcastic_since_2017-10-31_until_2017-12-05']
+				'#sarcastic_since_2017-10-31_until_2017-12-05',
+				'#sarcastic_since_2017-12-05_until_2017-12-14',
+				]
+
+dataFilesII = ['twitDB_sarcasm']
+dataFilesIII = ['twitDB_regular']
 
 dataSarcasm = []
-dataTotal = []
+dataNotSarcasm = []
 
 def strip_links(text):
     link_regex    = re.compile('((http?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
@@ -41,6 +39,7 @@ def strip_all_entities(text):
     return ' '.join(words)
 
 preprocessor.set_options(preprocessor.OPT.URL, preprocessor.OPT.HASHTAG, preprocessor.OPT.MENTION)
+
 def findClean(dataPt, switch):
 	if switch:
 		return preprocessor.clean(dataPt)
@@ -54,41 +53,65 @@ def findClean(dataPt, switch):
 			#return strip_all_entities(strip_links(dataPt))
 			#return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",strip_all_entities(dataPt)).split())
 
-for dataFile in dataFilesI:
+print "partI"
+for dataFile in dataFiles:
 	with open(DATADIR + dataFile + ".csv", 'rb') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',')
 		ct = 0
 		for row in reader:
-			# skip header
-			# ['id', 'screen_name', 'utc_offset', 'description', 'location', 'created_at', 'text', 'retweet_count', 'favorite_count']
+			# # skip header
+			# # ['id', 'screen_name', 'utc_offset', 'description', 'location', 'created_at', 'text', 'retweet_count', 'favorite_count']
 			if(ct == 0):
 				ct += 1
 				continue
-			# if description field contains "sarcasm" or "sarcastic" clean it (remove #hashtag) and add it to data
-			# print row[3]
-			# print row[6]
 			cleanData = findClean(row[3],False)
 			# clean description feild (remove #hashtag) and add it to data
-			cleanDataI = findClean(row[3],True)
 			if cleanData:
 				dataSarcasm.append(cleanData)
-			if cleanDataI:
-				dataTotal.append(cleanDataI)
 			
 			# if text field contains "sarcasm" or "sarcastic" clean it (remove #hashtag) and add it to data
 			cleanData = findClean(row[6],False)
 			# clean text field (remove #hashtag) and add it to data
-			cleanDataI = findClean(row[6],True)
 			if cleanData:
 				dataSarcasm.append(cleanData)
-			if cleanDataI:
-				dataTotal.append(cleanDataI)
-		
-		with open(DATADIR + dataFile + "cleaned.txt",'wb') as textfile:
-			for sentences in dataSarcasm:
-  				textfilels.write("%s\n" % sentences)
 
-		with open(DATADIR + dataFile + "cleaned.csv",'wb') as resultFile:
-			wr = csv.writer(resultFile)
-			for sentences in dataSarcasm:
-				wr.writerow([sentences])
+print "partII"			
+with open(DATADIR + dataFilesII[0] + ".csv", 'rU') as csvfile:
+	reader = csv.reader(csvfile, dialect=csv.excel_tab)
+	for row in reader:
+		if row:
+			dataSarcasm.append(preprocessor.clean(row[0]))
+
+print "partIII"
+with open(DATADIR + dataFilesIII[0] + ".csv", 'rU') as csvfile:
+	reader = csv.reader(csvfile, dialect=csv.excel_tab)
+	for row in reader:
+		if row:
+			dataNotSarcasm.append(preprocessor.clean(row[0]))
+	
+print "partIV"
+dataSarcasm = list(set(dataSarcasm))
+print "partV"
+dataNotSarcasm = list(set(dataNotSarcasm))
+
+#print dataSarcasm
+#print dataNotSarcasm
+
+
+with open(DATADIR + "sarcasm" + "cleaned.txt",'wb') as textfile:
+	for sentences in dataSarcasm:
+		textfile.write("%s\n" % sentences)
+
+with open(DATADIR + "sarcasm" + "cleaned.csv",'wb') as resultFile:
+	wr = csv.writer(resultFile)
+	for sentences in dataSarcasm:
+		wr.writerow([sentences])
+
+with open(DATADIR + "notsarcasm" + "cleaned.txt",'wb') as textfile:
+	for sentences in dataNotSarcasm:
+		textfile.write("%s\n" % sentences)
+
+with open(DATADIR + "notsarcasm" + "cleaned.csv",'wb') as resultFile:
+	wr = csv.writer(resultFile)
+	for sentences in dataNotSarcasm:
+		wr.writerow([sentences])
